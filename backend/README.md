e62fa31af2c24e0707c8ddd8c7195e7a63adc4dd
Geziel Carvalho <geziel.natal@gmail.com>
2024-03-06 20:51:51 -0500

    Initial setup and swagger documentation #8

---
+++ backend/README.md
@@ -0,0 +1,12 @@
+
+uvicorn main:app --reload
+https://github.com/faraday-academy/fast-api-lms
+https://github.com/faraday-academy
+https://fastapi.tiangolo.com/
+https://python-poetry.org/
+poetry add [lib name]
+
+
+https://www.youtube.com/watch?v=gQTRsZpR7Gw
+https://www.youtube.com/watch?v=0_seNFCtglk
+https://www.techempower.com/benchmarks/#hw=ph&test=fortune&section=data-r22
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,48 @@
-from fastapi import FastAPI
+from typing import Optional, List
+from fastapi import FastAPI, Path, Query, HTTPException
+from pydantic import BaseModel
+from starlette.responses import RedirectResponse
 
-app = FastAPI()
+app = FastAPI(
+    title="Aero-Zone Guard",
+    description="Analysis of construction projects impact on Aviation Safety",
+    version="1.0.0",
+    terms_of_service="https://gezielcarvalho.info/terms/",
+    contact={"name": "Geziel Carvalho", "email": "geziel.natal@gmail.com"},
+    license_info={
+        "name": "MIT",
+    }
+)
+
+users = []
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class User(BaseModel):
+    email: str
+    is_active: bool
+    bio: Optional[str]
+
+
+@app.get("/", include_in_schema=False)
+def read_root():
+    return RedirectResponse(url='/docs')
+
+
+@app.get("/users", response_model=List[User])
+async def get_users():
+    return users
+
+
+@app.post("/users")
+async def create_user(user: User):
+    users.append(user)
+    return "Success!!"
+
+
+@app.get("/users/{user_id}")
+async def read_user(
+        user_id: int = Path(..., description="The ID of the user you want to read"),
+        is_active: bool = Query(None)):
+    if users[user_id].is_active != is_active:
+        raise HTTPException(status_code=403, detail="User is "+("", " not ")[is_active]+" active")
+    return {"user": users[user_id], "is_active": is_active}
