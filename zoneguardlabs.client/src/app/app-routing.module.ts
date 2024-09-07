8bd52f099978167dd1e74286624bd15424dd6bd2
Geziel Carvalho <geziel.natal@gmail.com>
2024-09-06 20:55:10 -0400

    refactor: Update Angular project structure and add SubmissionDocument component
    - Refactor the Angular project structure to improve organization and maintainability
    - Add the SubmissionDocument component to handle document submissions
    
    # Details
    - Removed unused frontend files and configurations
    - Updated the app-routing.module.ts file to include the SubmissionDocument component
    - Added the AppRoutingModule constructor and test methods
    - Updated the app.component.ts file to remove unnecessary code
    - Updated the submission-document.component.html file to fix a closing div tag
    - Updated the proxy.conf.js file to include the root path ("/") for proxying requests

--- zoneguardlabs.client/src/app/app-routing.module.ts
+++ zoneguardlabs.client/src/app/app-routing.module.ts
@@ -4,10 +4,28 @@ import { SubmissionDocumentComponent } from './submission-document/submission-do
 
 const routes: Routes = [
   { path: 'submission-document', component: SubmissionDocumentComponent }
-];
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule
+{
+  constructor()
+  {
+    console.log("AppRoutingModule constructor");
+  };
+
+  test = () => {
+    console.log("test");
+  }
+
+  test2 = () => {
+    console.log("test2"); 
+  }
+
+  test3 = () => {
+    console.log("test3")
+  }
+}
