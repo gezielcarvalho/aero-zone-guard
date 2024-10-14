793b45337a5553e2cd05beafd2b205d6aaa3feff
Geziel Carvalho <geziel.natal@gmail.com>
2024-10-14 08:08:27 -0400

    Refactor SubmissionDocumentsController.cs to use ISubmissionDocumentRepository interface

--- AeroZoneGuard.Server/Controllers/SubmissionDocumentsController.cs
+++ AeroZoneGuard.Server/Controllers/SubmissionDocumentsController.cs
@@ -1,43 +1,33 @@
-﻿using AeroZoneGuard.Server.Data;
-using AeroZoneGuard.Server.Interfaces;
+﻿using AeroZoneGuard.Server.Interfaces;
 using AeroZoneGuard.Server.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace AeroZoneGuard.Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class SubmissionDocumentsController : ControllerBase
+    public class SubmissionDocumentsController(ISubmissionDocumentRepository repository) : ControllerBase
     {
-        private readonly AppDbContext _context;
-        private readonly ISubmissionDocumentRepository _repository;
-
-        public SubmissionDocumentsController(AppDbContext context, ISubmissionDocumentRepository repository)
-        {
-            _context = context;
-            _repository = repository;
-        }
 
         // GET: api/SubmissionDocuments
         [HttpGet]
         public async Task<ActionResult<IEnumerable<SubmissionDocument>>> GetSubmissionDocuments()
         {
-            return await _context.SubmissionDocuments.ToListAsync();
+            return Ok(await repository.GetAll());
         }
 
         // GET: api/SubmissionDocuments/5
         [HttpGet("{id}")]
         public async Task<ActionResult<SubmissionDocument>> GetSubmissionDocument(int id)
         {
-            var submissionDocument = await _context.SubmissionDocuments.FindAsync(id);
+            var submissionDocument = await repository.GetById(id);
 
             if (submissionDocument == null)
             {
                 return NotFound();
             }
 
-            return submissionDocument;
+            return Ok(submissionDocument);
         }
 
         // PUT: api/SubmissionDocuments/5
@@ -50,22 +40,13 @@ namespace AeroZoneGuard.Server.Controllers
                 return BadRequest();
             }
 
-            _context.Entry(submissionDocument).State = EntityState.Modified;
-
             try
             {
-                await _context.SaveChangesAsync();
+                await repository.Update(id, submissionDocument);
             }
-            catch (DbUpdateConcurrencyException)
+            catch (KeyNotFoundException)
             {
-                if (!SubmissionDocumentExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return NotFound();
             }
 
             return NoContent();
@@ -75,29 +56,20 @@ namespace AeroZoneGuard.Server.Controllers
         [HttpPost]
         public async Task<ActionResult<SubmissionDocument>> PostSubmissionDocument(SubmissionDocument submissionDocument)
         {
-            await _repository.Create(submissionDocument);
-            return CreatedAtAction("GetSubmissionDocument", new { id = submissionDocument.Id }, submissionDocument);
+            await repository.Create(submissionDocument);
+            return CreatedAtAction(nameof(GetSubmissionDocument), new { id = submissionDocument.Id }, submissionDocument);
         }
 
         // DELETE: api/SubmissionDocuments/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteSubmissionDocument(int id)
         {
-            var submissionDocument = await _context.SubmissionDocuments.FindAsync(id);
-            if (submissionDocument == null)
+            if (!await repository.Delete(id))
             {
                 return NotFound();
             }
-
-            _context.SubmissionDocuments.Remove(submissionDocument);
-            await _context.SaveChangesAsync();
-
             return NoContent();
         }
 
-        private bool SubmissionDocumentExists(int id)
-        {
-            return _context.SubmissionDocuments.Any(e => e.Id == id);
-        }
     }
 }
