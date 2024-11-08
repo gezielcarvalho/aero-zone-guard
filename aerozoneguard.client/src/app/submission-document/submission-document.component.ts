c3897318e8984d10d4c2ee9ae8fbbf3d618b33ec
Geziel Carvalho <geziel.natal@gmail.com>
2024-11-07 21:58:31 -0500

    Add submission date and last updated fields to submission document creation

--- aerozoneguard.client/src/app/submission-document/submission-document.component.ts
+++ aerozoneguard.client/src/app/submission-document/submission-document.component.ts
@@ -26,8 +26,22 @@ export class SubmissionDocumentComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log({ doc: this.submissionDocument })
+    // Set the submissionDate and lastUpdated to the current date and time
+    const currentDate = new Date().toISOString(); // ISO format for consistency
+    this.submissionDocument.submissionDate = currentDate;
+    this.submissionDocument.lastUpdated = currentDate;
+    this.submissionDocument.userId = 1; // temporary place holder
+
+    console.log({ doc: this.submissionDocument });
+
+    this.service.createSubmissionDocument(this.submissionDocument)
+      .subscribe({
+        next: (value) => { console.log(value); },
+        error: (error) => { console.error(error); },
+        complete: () => { console.log('Complete'); }
+      });
   }
+
 
   
 }
