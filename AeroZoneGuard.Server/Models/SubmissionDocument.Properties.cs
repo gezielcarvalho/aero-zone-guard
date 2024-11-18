ec94cb11a3f27973ce42dd47a49fd1418f4566aa
Geziel Carvalho <geziel.natal@gmail.com>
2024-11-17 21:18:35 -0500

    94. ASP.NET validation with data annotations

--- AeroZoneGuard.Server/Models/SubmissionDocument.Properties.cs
+++ AeroZoneGuard.Server/Models/SubmissionDocument.Properties.cs
@@ -1,10 +1,15 @@
-﻿namespace AeroZoneGuard.Server.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AeroZoneGuard.Server.Models
 {
     public partial class SubmissionDocument
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "User is required")]
         public int UserId { get; set; }
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
         public string Status { get; set; }
         public DateTime SubmissionDate { get; set; }
